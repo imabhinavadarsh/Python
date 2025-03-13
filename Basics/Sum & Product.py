fec6ec72221d2b78d212be23ff8a2ca4b5921ee0
Abhinav Adarsh <114936367+imabhinavadarsh@users.noreply.github.com>
2025-03-13 16:12:47 +0530

    Create Sum & Product.py
    Taken two variable as input from user and perform the sum and product.

---
+++ Basics/Sum & Product.py
@@ -0,0 +1,5 @@
+#WAP take two input from use and peform SUM AND PRODUCT
+n1 = int(input("Enter the first number :"))
+n2 = int(input("Enter the second number :"))
+print("Sum is :", n1 + n2)
+print("Product is :", n1 * n2)

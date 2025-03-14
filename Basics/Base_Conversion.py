ef94c918898fa90b7f2b9a0c2685002192e9467d
Abhinav Adarsh <114936367+imabhinavadarsh@users.noreply.github.com>
2025-03-14 14:49:18 +0530

    Create Base_Conversion.py
    Base conversion functions

---
+++ Basics/Base_Conversion.py
@@ -0,0 +1,7 @@
+#Base Conversion : In python there are three inbuilt function for Base conversion 
+# bin(), hex(), oct()
+
+n1 = int(input("Enter a number: "))
+print(hex(n1))
+print(bin(n1))
+print(oct(n1))
